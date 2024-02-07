2d505e0ce13e78528f82c0c997ebbff112c57383
cassanof <federico.cassano@federico.codes>
2024-02-07 17:25:50 -0500

    filters

--- get_metadata.py
+++ get_metadata.py
@@ -7,11 +7,12 @@ ds = datasets.load_dataset("CCRss/arXiv_dataset", split="train")
 # only keep "id"
 
 
-# filter category down to cs: "cs."
+# filter category down to cs: "cs." and no papers older than 2021
 print("pre-filter")
 print(ds)
 # would be nice to filter for citations; lots of papers are garbage
-ds = ds.filter(lambda x: "cs." in x["categories"], num_proc=os.cpu_count())
+# NOTE: dates are in datetime
+ds = ds.filter(lambda x: "cs." in x["categories"] and x["update_date"] > "2021-01-01")
 print("post-filter")
 print(ds)
 # care only about "id"
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,46 @@
+use async_compression::futures::bufread::GzipDecoder;
+use async_tar::Archive; // For handling tar archives
+use futures::{AsyncReadExt, StreamExt}; // For gzip decompression
+use std::{error::Error, io::Read};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() {}
+
+pub struct TexFile {
+    name: String,
+    content: String,
+}
+
+// the asynchronous function to download and extract TeX files
+async fn download_paper(id: &str) -> Result<Vec<TexFile>, Box<dyn Error>> {
+    let url = format!("https://arxiv.org/e-print/{}.pdf", id);
+    let response = reqwest::get(&url).await?;
+    let bytes = response.bytes().await?;
+
+    // find out if the file is gzipped
+    let is_gzipped = bytes.starts_with(&[0x1f, 0x8b]);
+    assert!(is_gzipped, "File is not gzipped?!?!?!");
+
+    // decompress gzip
+    let mut decoder = GzipDecoder::new(&bytes[..]);
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed).await?;
+    println!("Decompressed {} bytes", decompressed.len());
+
+    // extract tar
+    let archive = Archive::new(decompressed.as_slice());
+    let mut files = Vec::new();
+    let mut entries = archive.entries()?;
+    while let Some(file) = entries.next().await {
+        let mut file = file?;
+        let name = file.path()?.to_str().unwrap().to_string();
+        if !name.ends_with(".tex") {
+            // only tex allowed here!
+            continue;
+        }
+        let mut content = String::new();
+        file.read_to_string(&mut content).await?;
+        files.push(TexFile { name, content });
+    }
+    Ok(files)
 }
