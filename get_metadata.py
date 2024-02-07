94ea62d66416bc9e5924fbaec0f7ab3e406509ac
cassanof <federico.cassano@federico.codes>
2024-02-07 16:47:33 -0500

    map

--- get_metadata.py
+++ get_metadata.py
@@ -8,6 +8,11 @@ ds = datasets.load_dataset("CCRss/arXiv_dataset", split="train")
 # filter category down to cs: "cs."
 print("pre-filter")
 print(ds)
+# would be nice to filter for citations; lots of papers are garbage
 ds = ds.filter(lambda x: "cs." in x["categories"], num_proc=os.cpu_count())
 print("post-filter")
+print(ds)
+
+# care only about "id"
+ds = ds.map(lambda x: x["id"], num_proc=os.cpu_count())
 print(ds)
